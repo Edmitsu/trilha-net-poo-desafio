4c43225a7b51becf772fc07e687b1570b39f09a1
Edmitsu <ed.barcauscas@gmail.com>
2025-08-07 17:38:20 -0300

    Feature: adicionando todos atributos descritos no UML e sobrescrevendo o método nas classes de NOKIA e IPHONE.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("--- Teste Nokia ---");
+Nokia nokia = new Nokia(numero: "11999999999", imei: "123456789012345", modelo: "Nokia 3310", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+// Testes com a classe Iphone
+Console.WriteLine("\n--- Teste iPhone ---");
+Iphone iphone = new Iphone(numero: "11988888888", imei: "987654321098765", modelo: "iPhone 13", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
